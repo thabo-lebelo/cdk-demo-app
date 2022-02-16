99d4178b6b9b838ea1506e58ef3c8d568b74fac3
Thabo Lebelo <thabolebelo@icloud.com>
2022-02-16 23:41:20 +0200

    Add cloudfron stack

--- bin/cdk-demo-app.ts
+++ bin/cdk-demo-app.ts
@@ -2,8 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from "@aws-cdk/core";
 import { FargateDemoStack } from '../lib/fargate';
+import { CloudfrontDemoStack } from '../lib/cloudfront';
 
 const app = new cdk.App();
+
 new FargateDemoStack(app, 'CdkDemoAppStack', {
    env: { account: '737327749629', region: 'af-south-1' },
+});
+
+new CloudfrontDemoStack(app, "CloudfrontDemoStack", {
+  stage: "prod",
+  env: { account: "737327749629", region: "af-south-1" },
 });
